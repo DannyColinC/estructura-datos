693c5327d4684be450e3ea05ed6fbe257297bfe9
DannyColinC <30904885+DannyColinC@users.noreply.github.com>
2017-10-16 00:02:59 -0500

    Práctica 07 - Pilas y Filas
    Implementación de Expresiones Postfijas.

--- Queue.java
+++ Queue.java
@@ -1,5 +1,54 @@
-package Queue;
+package practica07;
+import java.util.NoSuchElementException;
 
-public class Queue {
+public class Queue<E> implements IQueue<E> {
+	private LinkedList<E> queue;
+	
+	public Queue() {
+		queue = new LinkedList<E>();
+	}
+	
+	@Override
+	public void offer(E e) {
+		queue.addLast(e);
+	}
+
+	@Override
+	public E remove() {
+		if(queue.isEmpty()) {
+			throw new NoSuchElementException();
+		}
+			
+		
+		return queue.removeLast();
+	}
+
+	@Override
+	public E element() {
+		if(queue.isEmpty())
+			throw new NoSuchElementException();
+		
+		return queue.getFirst();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return queue.isEmpty();
+	}
+
+	@Override
+	public void clear() {
+		queue.clear();
+	}
+
+	@Override
+	public int size() {
+		return queue.size();
+	}
+
+	@Override
+	public Object[] toArray() {
+		return queue.toArray();
+	}
 
 }
